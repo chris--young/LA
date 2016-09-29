3d130b3c2bbd13ef5073f4a00b709e62a6e1c11b
Chris Young <chris.young@gotinder.com>
2016-09-28 19:43:26 -0700

    Removed an alternative algorithm I had started with and cleaned up the tests.

--- la.js
+++ la.js
@@ -24,34 +24,6 @@
 
   _la.IDENTITY = Symbol();
 
-  const _vector2d = (t, x, y) => t === Array ? [x, y] : new t([x, y]);
-
-  _la.Vector2d = function (type) {
-    if (!~_la.TYPES.indexOf(type))
-      throw new TypeError();
-
-    return function (values) {
-      if (!values)
-        values = [0, 0];
-
-      return _vector2d(type, values[0], values[1]);
-    };
-  };
-
-  const _vector3d = (t, x, y, z) => t === Array ? [x, y, z] : new t([x, y, z]);
-
-  _la.Vector3d = function (type) {
-    if (!~_la.TYPES.indexOf(type))
-      throw new TypeError();
-
-    return function (values) {
-      if (!values)
-        values = [0, 0, 0];
-
-      return _vector3d(type, values[0], values[1], values[2]);
-    };
-  };
-
   function _vector(type, dimensions, values) {
     if (type === Array) {
       if (values)
@@ -82,10 +54,6 @@
     };
   };
 
-  _la.scale2d = (v, s) => _vector2d(v.constructor, v[0] * s, v[1] * s);
-
-  _la.scale3d = (v, s) => _vector3d(v.contrustor, v[0] * s, v[1] * s, v[2] * s);
-
   _la.scale = function (vector, scalar) {
     const v = _vector(vector.constructor, vector.length);
 
@@ -94,10 +62,6 @@
 
     return v;
   };
-
-  _la.add2d = (v0, v1) => _vector2d(v0.constructor, v0[0] + v1[0], v0[1] + v1[1]);
-
-  _la.add3d = (v0, v1, v2) => _vector3d(v0.constructor, v0[0] + v1[0] + v2[0], v0[1] + v1[1] + v2[1], v0[2] + v1[2] + v2[2]);
 
   _la.add = function (/* ...vectors */) {
     const v = _vector(arguments[0].constructor, arguments[0].length);
@@ -109,45 +73,13 @@
     return v;
   };
 
-  const _matrix2d = (t, v0, v1) => _vector2d(t, v0, v1);
-
-  _la.Matrix2d = function (type) {
-    if (!~_la.TYPES.indexOf(type))
-      throw new TypeError();
-
-    return function (values) {
-      if (!values)
-        values = [[0, 0], [0, 0]];
-
-      return values === _la.IDENTITY ? _identity2d(type) : _matrix2d(type, values[0], values[1]); 
-    };
-  };
-
-  const _matrix3d = (t, v0, v1, v2) => _vector3d(t, v0, v1, v2);
-
-  _la.Matrix3d = function (type) {
-    if (!~_la.TYPES.indexOf(type))
-      throw new TypeError();
-
-    return function (values) {
-      if (!values)
-        values = [[0, 0, 0], [0, 0, 0]];
-
-      return values === _la.IDENTITY ? _identity3d(type) : _matrix3d(type, values[0], values[1], values[2]); 
-    };
-  };
-
   function _matrix(type, dimensions, values) {
-    if (type === Array) {
-      const m = [];
+    const m = [];
 
-      for (let x = 0; x < dimensions; x++)
-        m[x] = _vector(type, dimensions, values && values[x]);
+    for (let x = 0; x < dimensions; x++)
+      m[x] = _vector(type, dimensions, values && values[x]);
 
-      return m;
-    }
-
-    return new ArrayBuffer(type.byteLength * dimensions);
+    return m;
   }
 
   _la.Matrix = function (type) {
@@ -164,10 +96,6 @@
     };
   };
 
-  const _identity2d = t => _matrix2d(t, [1, 0], [0, 1]);
-
-  const _identity3d = t => _matrix3d(t, [1, 0, 0], [0, 1, 0], [0, 0, 1]);
-
   function _identity(type, dimensions) {
     const m = _matrix(type, dimensions);
 
@@ -177,10 +105,6 @@
     return m;
   }
 
-  _la.transform2d = (v, m) => _la.sum2d(_la.scale2d(m[0], v[0]), _la.scale2d(m[1], v[1]));
-
-  _la.transform3d = (v, m) => _la.sum3d(_la.scale3d(m[0], v[0]), _la.scale3d(m[1], v[1]), _la.scale3d(m[2], v[2]));
-
   _la.transform = function (vector, matrix) {
     const s = [];
 
@@ -189,10 +113,6 @@
 
     return _la.add.apply(null, s);
   }
-
-  _la.multiply2d = (m0, m1) => _matrix2d(m0[0].constructor, _la.transform2d(m1[0], m0), _la.transform2d(m1[1], m0));
-
-  _la.multiply3d = (m0, m1) => _matrix3d(m0[0].constructor, _la.transform3d(m1[0], m0), _la.transform3d(m1[1], m0), _la.transform3d(m1[2], m0));
 
   // This wastes memory, is precomputing values slower though? probably not
   function _multiply(matrix0, matrix1) {
