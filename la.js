a356be526179e5aaaac9f8ca86adb70eb54fb0a0
Chris Young <chris.young@gotinder.com>
2018-01-03 12:30:04 -0800

    Added optimized functions for 2D, 3D, and 4D

--- la.js
+++ la.js
@@ -2,7 +2,37 @@
 
   'use strict'
 
-  var _la = {};
+  var _2d = {};
+
+  _2d.vector = (x, y) => [x, y];
+  _2d.matrix = (v0, v1) => _2d.vector(v0, v1);
+  _2d.identity = _2d.matrix([1, 0], [0, 1]);
+  _2d.sum = (v0, v1) => _2d.vector(v0[0] + v1[0], v0[1] + v1[1]);
+  _2d.scale = (v, s) => _2d.vector(v[0] * s, v[1] * s);
+  _2d.transform = (v, m) => _2d.sum(_2d.scale(m[0], v[0]), _2d.scale(m[1], v[1]));
+  _2d.multiply = (m0, m1) => _2d.matrix(_2d.transform(m1[0], m0), _2d.transform(m1[1], m0));
+
+  var _3d = {};
+
+  _3d.vector = (x, y, z) => [x, y, z];
+  _3d.matrix = (v0, v1, v2) => _3d.vector(v0, v1, v2);
+  _3d.identity = _3d.matrix([1, 0, 0], [0, 1, 0], [0, 0, 1]);
+  _3d.sum = (v0, v1, v2) => _3d.vector(v0[0] + v1[0] + v2[0], v0[1] + v1[1] + v2[1], v0[2] + v1[2] + v2[2]);
+  _3d.scale = (v, s) => _3d.vector(v[0] * s, v[1] * s, v[2] * s);
+  _3d.transform = (v, m) => _3d.sum(_3d.scale(m[0], v[0]), _3d.scale(m[1], v[1]), _3d.scale(m[2], v[2]));
+  _3d.multiply = (m0, m1) => _3d.matrix(_3d.transform(m1[0], m0), _3d.transform(m1[1], m0), _3d.transform(m1[2], m0));
+
+  var _4d = {};
+
+  _4d.vector = (x, y, z, w) => [x, y, z, w];
+  _4d.matrix = (v0, v1, v2, v3) => _4d.vector(v0, v1, v2, v3);
+  _4d.identity = _4d.matrix([1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]);
+  _4d.sum = (v0, v1, v2, v3) => _4d.vector(v0[0] + v1[0] + v2[0] + v3[0], v0[1] + v1[1] + v2[1] + v3[1], v0[2] + v1[2] + v2[2] + v3[2], v0[3] + v1[3] + v2[3] + v3[3]);
+  _4d.scale = (v, s) => _4d.vector(v[0] * s, v[1] * s, v[2] * s, v[3] * s);
+  _4d.transform = (v, m) => _4d.sum(_4d.scale(m[0], v[0]), _4d.scale(m[1], v[1]), _4d.scale(m[2], v[2]), _4d.scale(m[3], v[3]));
+  _4d.multiply = (m0, m1) => _4d.matrix(_4d.transform(m1[0], m0), _4d.transform(m1[1], m0), _4d.transform(m1[2], m0), _4d.transform(m1[3], m0));
+
+  var _la = { _2d, _3d, _4d };
 
   if (typeof module !== 'undefined')
     module.exports = _la;
